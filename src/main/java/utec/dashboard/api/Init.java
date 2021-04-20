68be64c9d68b0ec31850a72e9254f88507593624
Miguel Escobar <moswaldoec01@gmail.com>
2021-04-20 09:48:30 -0600

    Modificacion de clase Init puerto dinamico

--- src/main/java/utec/dashboard/api/Init.java
+++ src/main/java/utec/dashboard/api/Init.java
@@ -11,11 +11,19 @@ public class Init {
     public static void main(String[] args) {
         Usuario user = new Usuario();
 //        get("/helloworld", (req, res) -> user.logearUsuarios("edwin.alberto", "p4ssw0rd1234") + req.queryParams("name"));
-        port(8080);
+        port(getHerokuAssignedPort());
 
         get("/validarUsuario", (req, res) -> {
             String s = user.logearUsuarios(req.queryParams("user"), req.queryParams("pwd"));
             return s;
         });
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
